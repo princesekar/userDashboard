1bb4436b30f7b898ed5a974c309ed25418a6f434
prince sekar <princesekar85@gmail.com>
2024-06-08 03:42:09 +0530

    data.json

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core'
 import { SharedService } from '../shared.service';
 @Component({
   selector: 'app-dashboard',
@@ -7,19 +7,23 @@ import { SharedService } from '../shared.service';
 })
 export class DashboardComponent implements OnInit {
   profile: any;
-  public leaveChartLabels: number[] = Array.from({ length: 30 }, (_, i) => i + 1);
-  constructor(private sharedService: SharedService) { }
+  interval: any;
+  constructor(private sharedService: SharedService, private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.getUserProfile();
-    console.log(this.leaveChartLabels, 'leaveChartLabels')
+
+    this.interval = setInterval(() => {
+      this.getAlertsData();
+      this.changeDetectorRef.detectChanges();
+    }, 5000);
   }
 
   getUserProfile(): void {
     this.sharedService.fetchUserProfile().subscribe({
       next: (profile) => {
         this.profile = profile;
-        console.log(profile, 'fetched profile');
+        console.log(this.profile, 'this.profile profile');
       },
       error: (error) => {
         console.error('Error fetching profile:', error);
@@ -160,4 +164,94 @@ export class DashboardComponent implements OnInit {
       },
     },
   }
+
+
+
+  getAlertsData = () => {
+    
+    let taskData1 = this.profile.dashboard[0].task[0];
+    let taskData2 = this.profile.dashboard[0].task[1];
+    let leaveCData1= this.profile.dashboard[0].status[0];
+    let leaveCData2= this.profile.dashboard[0].status[1];
+    let doughnutForData = this.profile.dashboard[0].updateui;
+
+   let lineChartData = {
+      labels: ['1 Am', '2 Am', '3 Am', '4 AM', '5 AM', '6 AM', '7 AM'],
+      datasets: [
+        {
+          label: 'Today',
+          data: taskData1,
+          borderColor: '#F01D1D',
+          backgroundColor: '#f2b5ae',
+          borderWidth: 1,
+          pointRadius: 2,
+          pointBackgroundColor: '#F01D1D',
+          pointBorderColor: '#F01D1D',
+          fill: true
+        },
+        {
+          label: 'Yesterday',
+          data: taskData2,
+          borderColor: '#508b8c',
+          borderWidth: 1,
+          pointRadius: 2,
+          backgroundColor: 'rgba(110, 190, 191, 0.2)',
+          pointBackgroundColor: '#508b8c',
+          pointBorderColor: '#508b8c',
+          fill: true
+        },
+      ],
+    };
+  
+    let leaveChartData = {
+      labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+      datasets: [
+        {
+          label: 'Current Year ',
+          data: leaveCData1,
+          borderColor: '#F01D1D',
+          backgroundColor: '#f2b5ae',
+          borderWidth: 1,
+          pointRadius: 2,
+          pointBackgroundColor: '#F01D1D',
+          pointBorderColor: '#F01D1D',
+        },
+        {
+          label: 'Previous Year',
+          data: leaveCData2,
+          borderColor: '#508b8c',
+          borderWidth: 1,
+          pointRadius: 2,
+          backgroundColor: 'rgba(110, 190, 191, 0.2)',
+          pointBackgroundColor: '#508b8c',
+          pointBorderColor: '#508b8c',
+        },
+      ],
+    };
+  
+    let doughnutData = {
+      labels: [
+        'New',
+        'Progress',
+        'Completed',
+        'Canceled'
+      ],
+      datasets: [{
+        label: 'Projects',
+        data: doughnutForData,
+        backgroundColor: [
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+          'rgb(68, 131, 102)',
+          'rgb(255, 99, 132)',
+        ],
+        hoverOffset: 4
+      }]
+    };
+
+
+    return {
+      lineChartData, leaveChartData, doughnutData
+    };
+  };
 }
